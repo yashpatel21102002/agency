c6232b1f415fdfdaab0d68a86f4bb4ab9a55102c
yash patel <yashpatel21102002@gmail.com>
2024-06-02 08:46:43 +0530

    02062024 1

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import { hostname } from 'os';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['uploadthing.com', 'utfs.io', 'img.clerk.com', 'subdomain', 'files.stripe.com']
+        remotePatterns: hostname['uploadthing.com', 'utfs.io', 'img.clerk.com', 'subdomain', 'files.stripe.com']
     }
 };
 
